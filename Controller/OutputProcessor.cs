e5bc5cb48cae348aef8b28561446795fe8362302
eportilla <marsarf@gmail.com>
2020-01-25 11:15:00 -0800

    Created beta outputprocessor. Have not verified if it builds

--- Controller/OutputProcessor.cs
+++ Controller/OutputProcessor.cs
@@ -1,7 +1,30 @@
 namespace HashPhotoSlideshow.Controller
 {
+	using System.IO;
+	using System.Text;
+
     public class OutputProcessor
     {
+    	private string slideshowOutputFileName = $"slideshow_{Date.now()}";
+    	private Slideshow Slideshow {get;}
 
+    	public OutputProcessor(Slideshow slideshow) {
+    		Slideshow = slideshow;
+    	}
+
+    	public void ProcessSlideShow() {
+    		using (var fs = File.Create(slideshowOutputFileName)) {
+    			using (var sr = new StreamWriter(fs)) {
+	    			sr.WriteLine(Slideshow.size().ToString());
+
+	    			foreach (var slide in Slideshow) {
+	    				foreach (var photo in slide) {
+	    					sr.Write($"{photo.Id.ToString()} ");
+	    				}
+	    				sr.WriteLine();
+	    			}
+    			}
+    		}
+    	}
     }
 }
